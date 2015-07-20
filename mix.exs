1d607b171c0609a4cd3d7e57a822e1e5c9d1298b
Mike Binns <mike.binns@perceptivesoftware.com>
2015-07-20 16:32:29 -0400

    changed to porcelain

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ProcessTest.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :erlexec]]
+    [applications: [:logger, :porcelain]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule ProcessTest.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:erlexec, git: "https://github.com/saleyn/erlexec"}]
+    [{:porcelain, "~> 2.0"}]
   end
 end
