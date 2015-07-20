a09c1b32d078ffca649292cb23ba591c105b0e82
Mike Binns <mike.binns@perceptivesoftware.com>
2015-07-20 15:19:25 -0400

    a

--- mix.exs
+++ mix.exs
@@ -27,6 +27,6 @@ defmodule ProcessTest.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [:erlexec, git: "https://github.com/saleyn/erlexec"]
+    [{:erlexec, git: "https://github.com/saleyn/erlexec"}]
   end
 end
